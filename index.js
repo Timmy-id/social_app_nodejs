d2f80e6c48b2be16597264b076814e569737f189
Oluwatimilehin Oreoluwa Idowu <oluwatimilehin.id@gmail.com>
2021-10-19 21:18:16 +0100

    create the user model and the auth route

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL,
@@ -16,6 +19,9 @@ mongoose.connect(process.env.MONGO_URL,
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('common'));
+
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
 
 app.listen(8000, () => {
     console.log('server working');
